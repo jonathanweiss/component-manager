ab52df53a5321c4f91052671f4972add343093ef
Jonathan Weiß <bitbucket@jonathanweiss.net>
2016-08-09 10:01:12 +0200

    Improve API documentation

--- src/manager.js
+++ src/manager.js
@@ -13,6 +13,11 @@
   var _registeredComponents = {};
   var _observer = null;
 
+  /**
+   * Reacts to changes to the DOM and calls the designated callbacks to create or delete instances
+   * @param {Array} A list of {@link https://developer.mozilla.org/en-US/docs/Web/API/MutationRecord|MutationRecords}
+   * @return {undefined}
+   */
   var _onDomChange = function (mutations) {
     mutations.forEach(function (mutation) {
       var addedComponents = $(mutation.addedNodes).find('[data-component-name]');
@@ -39,7 +44,15 @@
     });
   };
 
-  var register = function (name, priority, callbackAdd, callbackRemove) {
+  /**
+   * Registers a component
+   * @param {String} selector Selector to identify the component
+   * @param {Number} priority Order in which the components will be initialised
+   * @param {Function} callbackAdd Function that will be executed when a DOM node matching the selector is added to the document
+   * @param {Function} callbackRemove Function that will be executed when a DOM node matching the selector is removed from the document
+   * @return {undefined}
+   */
+  var register = function (selector, priority, callbackAdd, callbackRemove) {
     _registeredComponents[name] = {
       priority: priority,
       onAdd: callbackAdd,
@@ -49,7 +62,7 @@
 
   var init = function () {
     _observer = new MutationObserver(_onDomChange);
-    _observer.observe($('body').get(0), {
+    _observer.observe(document.body.get(0), {
       childList: true,
       subtree: true
     });
