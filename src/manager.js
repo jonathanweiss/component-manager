48cc064e3df5ee44bb9a5c2a42a6f8b23fc3d0a9
Jonathan Weiß <bitbucket@jonathanweiss.net>
2016-09-07 18:39:18 +0200

    Add optional parameter "prioroty" to register()

--- src/manager.js
+++ src/manager.js
@@ -1,4 +1,4 @@
-/*! ComponentManager - v1.0.0
+/*! ComponentManager - v1.1.0
  * https://github.com/jonathanweiss/component-manager
  * Copyright (c) 2016 https://github.com/jonathanweiss/component-manager/authors.txt
  * License: MIT
@@ -29,13 +29,18 @@
 
             [].slice.call(mutation.addedNodes).forEach(function(addedNode) {
                 if (addedNode.tagName) {
-                    [].slice.call(addedNode.querySelectorAll('[' + DATA_ATTRIBUTE + ']')).forEach(function(specificNode) {
-                        var componentName = specificNode.getAttribute(DATA_ATTRIBUTE);
-                        if (registeredComponents[componentName]) {
-                            registeredComponents[componentName].onAdd(specificNode);
-                        }
-                    });
-                }
+                    [].slice.call(addedNode.querySelectorAll('[' + DATA_ATTRIBUTE + ']'))
+                        .sort(function(a, b) {
+                            return registeredComponents[a.getAttribute('data-component-name')].priority >
+                                   registeredComponents[b.getAttribute('data-component-name')].priority;
+                        })
+                        .forEach(function(specificNode) {
+                            var componentName = specificNode.getAttribute(DATA_ATTRIBUTE);
+                            if (registeredComponents[componentName]) {
+                                registeredComponents[componentName].onAdd(specificNode);
+                            }
+                        });
+                    }
             });
 
             [].slice.call(mutation.removedNodes).forEach(function(removedNode) {
@@ -57,13 +62,17 @@
     * @param {String} name Name to identify the component; is used as a data attribute on target nodes
     * @param {Function} callbackAdd Function that will be executed when a DOM node matching the selector is added to the document
     * @param {[Function=undefined]} callbackRemove Function that will be executed when a DOM node matching the selector is removed from the document
+    * @param {[Number=100]} priority Important of the component with 0 being the most important.
     * @return {undefined}
     */
-    var register = function(name, callbackAdd, callbackRemove) {
-        registeredComponents[name] = {
-            onAdd: callbackAdd,
-            onRemove: callbackRemove ? callbackRemove : function(){}
-        };
+    var register = function(name, callbackAdd, callbackRemove, priority) {
+        if (!registeredComponents[name]) {
+            registeredComponents[name] = {
+                onAdd: callbackAdd,
+                onRemove: callbackRemove ? callbackRemove : function(){},
+                priority: priority || 100
+            };
+        }
     };
 
     /**
